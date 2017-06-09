c39cf52f4f941f685d15cd826a8f52e1286f6baa
kuro <kuro@server.home>
2017-06-10 00:48:16 +0800

    -> initialize AzureCloudService.GettingStarted

---
+++ AzureCloudService.Examples/AzureCloudService.GettingStarted.WebRole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureCloudService.GettingStarted.WebRole.WebApiApplication" Language="C#" %>
---
+++ AzureCloudService.Examples/AzureCloudService.GettingStarted.WorkerRole.Tests/WorkerRole_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureCloudService.GettingStarted.WorkerRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureCloudService.GettingStarted.WorkerRole.Tests
+{
+    [TestClass()]
+    public class WorkerRole_Tests
+    {
+        [TestMethod()]
+        public void Run_Test()
+        {
+
+        }
+    }
+}
